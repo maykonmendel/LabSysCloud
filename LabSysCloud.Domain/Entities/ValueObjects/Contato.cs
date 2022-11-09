2dd2a49daeb185c7eac471fb42bb92af26c50e26
Maykon Mendel <maykonmendel.dev@gmail.com>
2022-11-09 08:23:06 -0300

    Remoção de campos desnecessários

--- LabSysCloud.Domain/Entities/ValueObjects/Contato.cs
+++ LabSysCloud.Domain/Entities/ValueObjects/Contato.cs
@@ -5,11 +5,9 @@ namespace LabSysCloud.Domain.Entities.ValueObjects
 {
     [Owned]
     public class Contato
-    {        
+    {
         public string Email { get; set; }
-        public int DDDResidencial { get; set; }
         public string TelefoneResidencial { get; set; }
-        public int DDDCelular { get; set; }
         public string Celular { get; set; }
     }
 }
